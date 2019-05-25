0cd0211c9fb54834899d6e0f79ccab743c83f8ad
Kaiyu <skyisno.1@gmail.com>
2019-05-24 21:46:52 -0400

    Init commit from pip package

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n' +
+    'Download\n'
+    '========\n'
+    )
+
+setup(
+    name="calmsize",
+    version="0.9.1",
+    description="Fork from hurry.filesize, A simple Python library for human readable file sizes (or anything sized in bytes).",
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    keywords='file size bytes',
+    author='Kaiyu Shi',
+    author_email='skyisno.1@gmail.com',
+    url='',
+    license='ZPL 2.1',
+    packages=find_packages('calmsize'),
+    package_dir= {'': 'calmsize'},
+    # namespace_packages=['calmsize'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+)
